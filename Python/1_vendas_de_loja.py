edeb104421be9814db2054796f1d72fea2c4e60f
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-21 22:14:09 -0300

    Create 1_vendas_de_loja.py

---
+++ Python/1_vendas_de_loja.py
@@ -0,0 +1,16 @@
+pedidos = [
+    {"cliente": "Ana", "valor": 120.0, "itens": 3},
+    {"cliente": "Bruno", "valor": 80.0, "itens": 1},
+    {"cliente": "Carlos", "valor": 200.0, "itens": 5},
+    {"cliente": "Ana", "valor": 50.0, "itens": 2},
+    {"cliente": "Bruno", "valor": 300.0, "itens": 6},
+]
+
+# Calcule o valor total de todos os pedidos.
+i = 0
+sum = 0
+while i<len(pedidos):
+    sum += pedidos[i]['valor']
+    i+= 1
+print(sum)
+
